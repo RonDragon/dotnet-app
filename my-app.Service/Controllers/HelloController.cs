2cab5957924278d00994eabecfd4c0aba436f3b5
Ronel David <48948654+RonDragon@users.noreply.github.com>
2024-06-03 12:40:02 +0000

    new image tag

--- my-app.Service/Controllers/HelloController.cs
+++ my-app.Service/Controllers/HelloController.cs
@@ -10,7 +10,7 @@ namespace my_app.Service.Controllers
         [Route("hello")]
         public IActionResult GetHello()
         {
-            return Ok("Hello, World! test ver 3");
+            return Ok("Hello, World! test ver 4");
         }
     }
 }
